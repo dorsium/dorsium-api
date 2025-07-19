b7dafba51fba40a115b0dad07c45cb07b591ed14
Grávuj Miklós Henrich <67593243+gravuj-miklos-henrich@users.noreply.github.com>
2025-07-19 18:36:50 +0300

    Remove extraneous comment

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -1,19 +1,18 @@
-// Modified by Name for: configure swagger plugin
 import { FastifyInstance } from 'fastify';
-import fastifySwagger from '@fastify/swagger';
+import fastifySwagger, { SwaggerOptions } from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
 
 export default async function swagger(app: FastifyInstance) {
-  await app.register(fastifySwagger, {
+  const swaggerOptions: SwaggerOptions = {
     openapi: {
       info: {
         title: 'Dorsium API',
         description: 'Dorsium API docs',
         version: '1.0.0'
       }
-    },
-    exposeRoute: process.env.NODE_ENV !== 'production'
-  });
+    }
+  };
+  await app.register(fastifySwagger, swaggerOptions);
 
   await app.register(fastifySwaggerUi, {
     routePrefix: '/docs',
