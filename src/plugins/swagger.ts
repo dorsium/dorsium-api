189651147ae970e96a40ad29742f6645404d5f08
Grávuj Miklós Henrich <67593243+gravuj-miklos-henrich@users.noreply.github.com>
2025-07-19 16:48:00 +0300

    Configure Swagger plugin

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -1,5 +1,25 @@
+// Modified by Assistant for: configure swagger plugin
 import { FastifyInstance } from 'fastify';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 export default async function swagger(app: FastifyInstance) {
-  // TODO: configure swagger
+  await app.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Dorsium API',
+        description: 'Dorsium API docs',
+        version: '1.0.0'
+      }
+    },
+    exposeRoute: true
+  });
+
+  await app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'list',
+      deepLinking: false
+    }
+  });
 }
